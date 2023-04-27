cca27781bc3c43f86afebfb1e377cf8f6962e59f
Haitham Dhouib <Dhouibhaitham@gmail.com>
2023-04-27 15:33:46 +0100

    Converter

--- dbconfig.php
+++ dbconfig.php
@@ -1,10 +1,7 @@
 <?php	
-	/*$servername = "localhost";
+	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$dbname = "rgg";*/
-	$servername = "135.148.33.157";
-	$username = "187";
-	$password = "DarioRP";
-	$dbname = "rgg";
+	$dbname = "";
+
 ?>
